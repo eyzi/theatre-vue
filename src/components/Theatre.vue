1d2872061d351e0e809bef50c06b17cd54850274
eyzi <contact@eyzi.dev>
2022-10-16 22:16:42 -0400

    feat(main): sample slides poc

--- src/components/Theatre.vue
+++ src/components/Theatre.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { ref, onMounted, computed, defineComponent, reactive } from "vue";
 import studio from "@theatre/studio";
-import { getProject, types as t } from "@theatre/core";
+import { getProject, types as t, onChange } from "@theatre/core";
 import {
   TorusKnot,
   Camera,
@@ -11,6 +11,10 @@ import {
   Renderer,
   Scene,
   StandardMaterial,
+  Box,
+  FbxModel,
+  BasicMaterial,
+  CubeTexture,
 } from "troisjs";
 
 defineComponent({
@@ -23,40 +27,118 @@ defineComponent({
     Renderer,
     Scene,
     StandardMaterial,
+    FbxModel,
+    BasicMaterial,
+    CubeTexture,
   },
 });
-
-const torusKnotRotation = reactive({
+const cameraPosition = reactive({
+  x: 0,
+  y: 0,
+  z: 10,
+});
+const cameraRotation = reactive({
+  x: 0,
+  y: 0,
+  z: 0,
+});
+const boxPosition = reactive({
+  x: 0,
+  y: 0,
+  z: -30,
+});
+const boxMetallness = 0;
+const boxRotation = reactive({
   x: 0,
   y: 0,
   z: 0,
 });
 
+const slideIndex = ref(0);
+const slides = [0, 1.5, 2];
+const waitingForNextSlide = ref(false);
+
+const project = getProject("TheatreVue");
+const sheet1 = project.sheet("Sheet1");
+const sheet2 = project.sheet("Sheet2");
+
 onMounted(() => {
   studio.initialize();
-  const project = getProject("TheatreVue");
-  const sheet = project.sheet("Sheet");
-  const torusKnotObj = sheet.object("Torus Knot", {
-    rotation: t.compound({
-      x: t.number(torusKnotRotation.x, { range: [-2, 2] }),
-      y: t.number(torusKnotRotation.y, { range: [-2, 2] }),
-      z: t.number(torusKnotRotation.z, { range: [-2, 2] }),
-    }),
-  });
-  torusKnotObj.onValuesChange((values) => {
-    const { x, y, z } = values.rotation;
-    torusKnotRotation.x = x * Math.PI;
-    torusKnotRotation.y = y * Math.PI;
-    torusKnotRotation.z = z * Math.PI;
+  const boxObj = sheet1
+    .object("Box", {
+      position: t.compound({
+        x: t.number(boxPosition.x),
+        y: t.number(boxPosition.y),
+        z: t.number(boxPosition.z),
+      }),
+      rotation: t.compound({
+        x: t.number(boxRotation.x, { range: [0, 20] }),
+        y: t.number(boxRotation.y, { range: [0, 20] }),
+        z: t.number(boxRotation.z, { range: [0, 20] }),
+      }),
+    })
+    .onValuesChange((values) => {
+      boxPosition.x = values.position.x;
+      boxPosition.y = values.position.y;
+      boxPosition.z = values.position.z;
+      boxRotation.x = (values.rotation.x % 2) * Math.PI;
+      boxRotation.y = (values.rotation.y % 2) * Math.PI;
+      boxRotation.z = (values.rotation.z % 2) * Math.PI;
+    });
+  const camObj = sheet2
+    .object("Camera", {
+      position: t.compound({
+        x: t.number(cameraPosition.x),
+        y: t.number(cameraPosition.y),
+        z: t.number(cameraPosition.z),
+      }),
+      rotation: t.compound({
+        x: t.number(cameraRotation.x, { range: [0, 20] }),
+        y: t.number(cameraRotation.y, { range: [0, 20] }),
+        z: t.number(cameraRotation.z, { range: [0, 20] }),
+      }),
+    })
+    .onValuesChange((values) => {
+      cameraPosition.x = values.position.x;
+      cameraPosition.y = values.position.y;
+      cameraPosition.z = values.position.z;
+    });
+  project.ready.then(() => {
+    sheet1.sequence.play({ range: [0, 1], iterationCount: Infinity });
+
+    window.addEventListener("keydown", (e) => {
+      if (e.key === "Enter") {
+        nextSlide();
+      }
+    });
   });
 });
+
+function previousSlide() {
+  if (slideIndex.value <= 0) {
+    slideIndex.value = 0;
+  } else {
+    slideIndex.value--;
+    sheet2.sequence.position = slides[slideIndex.value];
+  }
+}
+
+function nextSlide() {
+  const nextSlide = slideIndex.value + 1;
+  if (nextSlide >= slides.length) return;
+
+  slideIndex.value++;
+  sheet2.sequence.play({
+    range: [sheet2.sequence.position, slides[slideIndex.value]],
+  });
+}
 </script>
 
 <template>
   <Renderer id="renderer" ref="renderer" antialias resize>
     <Camera
       ref="camera"
-      :position="{ z: 10 }"
+      :position="cameraPosition"
       :aspect="1"
       :fov="70"
       :near="0.1"
@@ -83,26 +165,28 @@ onMounted(() => {
           left: -20,
         }"
       />
-      <TorusKnot
-        ref="torusKnot"
-        :position="{ z: -30 }"
-        :rotation="torusKnotRotation"
-        :radius="10"
-        :tube="3"
-        :tubularSegments="300"
-        :radialSegments="16"
-        castShadow
-        receiveShadow
-      >
+      <Box ref="box" :position="boxPosition" :size="10" :rotation="boxRotation">
         <StandardMaterial color="#049ef4" />
-      </TorusKnot>
+      </Box>
     </Scene>
   </Renderer>
+  <div id="slide-info">
+    <div>Slide: {{ slideIndex }}</div>
+    <button @click="previousSlide">&lt;</button>
+    <button @click="nextSlide">&gt;</button>
+  </div>
 </template>
 
 <style scoped>
 #renderer {
   width: 100%;
   height: 100%;
+}
+
+#slide-info {
+  position: absolute;
+  bottom: 5px;
+  left: 5px;
+  color: white;
 }
 </style>
